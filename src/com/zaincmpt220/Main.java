c6e5b1b47ed2f391f4cb7f2dbb1d90802d50e032
zainvideos <zainvideos@gmail.com>
2014-10-02 16:58:27 -0400

    Improvements
    By Zain on 10/2/2014

--- src/com/zaincmpt220/Main.java
+++ src/com/zaincmpt220/Main.java
@@ -14,24 +14,27 @@ public class Main {
         event.add("Cornhole");
         event.add("Ladderball");
         event.add("Stickgame");
-//welcome to the Matrix
+        //app welcome message
         System.out.println("Welcome To the Smith Family Olympics App. Enter a command or press h for help.");
-        String[][] olympian = {
-                { "Bob", "male", "41" },
-                { "Mike", "male", "13" },
-                { "Derk", "male", "23" },
-                { "Derky", "female", "21" },
-                { "Derp", "male", "53" },
-                { "Derpina", "female", "55" },
-                { "Merk", "male", "44" },
-                { "Derker", "male", "3" },
-                { "Berkmatic", "male", "26" },
-                { "Sudsy", "female", "29" },
-                { "Dork", "male", "93" },
-                { "Vern", "male", "33" },
-                { "Surpy", "female", "58" },
-                { "Brianna", "female", "32" },
-                { "Brian", "male", "32" },
+
+        //welcome to the Matrix
+     String[][] olympian;
+        olympian = new String[][]{
+                {"Bob", "male", "41"},
+                {"Mike", "male", "13"},
+                {"Derk", "male", "23"},
+                {"Derky", "female", "21"},
+                {"Derp", "male", "53"},
+                {"Derpina", "female", "55"},
+                {"Merk", "male", "44"},
+                {"Derker", "male", "3"},
+                {"Berkmatic", "male", "26"},
+                {"Sudsy", "female", "29"},
+                {"Dork", "male", "93"},
+                {"Vern", "male", "33"},
+                {"Surpy", "female", "58"},
+                {"Brianna", "female", "32"},
+                {"Brian", "male", "32"},
         };
 //alternatively, i could have done this but it would be too messy and long for 15 olympians:
         /*String[][] olympian = new String[16][3];
@@ -45,18 +48,18 @@ public class Main {
 //lets start with args.length so we can complain about blank entries right off the bat
          if(args.length ==0)
             System.out.println("You need to select an option.... are you feeling ok?");
-        else if (args[0].toString().equals("e")||args[0].toString().equals("events")) {
+        else if (args[0].equals("e")||args[0].equals("events")) {
             System.out.println(event);
              //we obviously need to loop through all the values in the olympian matrix to print them, using the standard
              //System.out.println(olympian); is just going to print out the memory location of the matrix lol
-    }else if(args[0].equals("o")||args[0].toString().equals("olympians")){
+    }else if(args[0].equals("o")||args[0].equals("olympians")){
             for (int i = 0; i < olympian.length; i++) {
                 for (int j = 0; j < 3; j++) {
                     System.out.println(olympian[i][j] + " ");
                 }
                 System.out.println("\n");
             }
-    }else if(args[0].equals("h")||args[0].toString().equals("help")) {
+    }else if(args[0].equals("h")||args[0].equals("help")) {
             System.out.println("Type e for list of events, or type o for a list of olympians");
     }
 //there are only 3 options, this isn/'t that hard...
